e79e2d85e8500db600aa31aca6a8fcecb9cdf93f
Ryan Sonnek <ryan@codecrate.com>
2014-09-09 17:25:46 +0200

    remove unused matcher

--- spec/support/meet_expectations_matcher.rb
+++
@@ -1,7 +0,0 @@
-# empty matcher to allow for mock expectations to fire
-RSpec::Matchers.define :meet_expectations do |expected|
-  match do |actual|
-    # do nothing
-    expect(true).to be_true
-  end
-end
